4b9b0ee9bef3f4f6879faae8c99431cef4851cbe
Giovanny Perez <789@holbertonschool.com>
2019-02-10 23:41:06 +0000

    Ultimo

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int unidades;
+	int decenas;
+	int uni2;
+	int dece2;
+	int contador;
+
+	for (decenas = 48 ; decenas <= 57 ; decenas++)
+		for (unidades = 48 ; unidades <= 57 ; unidades++)
+		{
+			for (dece2 = decenas ; dece2 <= 57 ; dece2++)
+				for (uni2 = unidades + 1 ; uni2 <= 57 ; uni2++)
+				{
+					putchar (decenas);
+					putchar (unidades);
+					putchar (' ');
+					putchar (dece2);
+					putchar (uni2);
+					if (decenas != 57 || unidades != 56 || dece2 != 57 || uni2 != 57)
+					{						
+						putchar (',');
+						putchar (' ');
+					}
+					else 
+						putchar ('\n');
+				}
+		}
+	return (0);
+}
