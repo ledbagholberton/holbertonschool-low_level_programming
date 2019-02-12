bbef658d01d2a471d78705e8e428f7000bd6e60c
Giovanny Perez <789@holbertonschool.com>
2019-02-12 15:42:17 +0000

    Libraries & others

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,16 @@
 #include "holberton.h"
 
+/**
+ * print_alphabet - print the alphabet in lowercase
+ *
+ * void: No input variables
+ * Return: No return
+ */
+
 void print_alphabet(void)
 {
 	char letter;
+
 	for (letter = 'a' ; letter <= 'z' ; letter++)
 		_putchar (letter);
 	_putchar('\n');
