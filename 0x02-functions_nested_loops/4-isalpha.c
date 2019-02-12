5d1d637c0ecadee4cf2af7389d1f7375750d95af
Giovanny Perez <789@holbertonschool.com>
2019-02-12 15:52:20 +0000

    Libraries & others

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,8 +1,14 @@
 #include "holberton.h"
 
+/**
+ * _isalpha - return 1 if input is a letter
+ *
+ * @c: Bring the character to evaluate
+ * Return: Return  if character is a letter; 0 in other cases
+ */
 int _isalpha(int c)
 {
-	if ((c >= 65 && c<=90) || (c >= 97 && c <= 122))
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 		return (1);
 	return (0);
 }
