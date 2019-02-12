5a271e45df46143e4b58351a05b0347d18b976d1
Giovanny Perez <789@holbertonschool.com>
2019-02-12 15:45:30 +0000

    Libraries & others

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,17 @@
 #include "holberton.h"
 
+/**
+ * print_alphabet_x10 - print the alphabet 10 times in lowercase
+ *
+ * void: No input variables
+ * Return: No return
+ */
 void print_alphabet_x10(void)
 {
 	int cont;
 	int letter;
 
-	for (cont = 1; cont <=10 ; cont++)
+	for (cont = 1; cont <= 10 ; cont++)
 	{
 		for (letter = 'a' ; letter <= 'z' ; letter++)
 			_putchar(letter);
