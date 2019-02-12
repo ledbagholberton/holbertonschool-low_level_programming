4b441c91564926584626bab3b02e7f78c9f133ce
Giovanny Perez <789@holbertonschool.com>
2019-02-12 15:21:39 +0000

    Libraries & others

---
+++ 0x02-functions_nested_loops/#3-islower.c#
@@ -0,0 +1,9 @@
+#include "holberton.h"
+
+int _islower(int c)
+
+{
+	if (c >= 90 && c <= 122)
+		return (1);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,9 @@
+#include "holberton.h"
+
+int _abs(int numero)
+{
+	if (numero<0)
+		return (numero*-1);
+	else
+		return (numero);
+}
