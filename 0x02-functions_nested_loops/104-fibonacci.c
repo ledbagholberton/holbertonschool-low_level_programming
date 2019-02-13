31e1a446fc42fbae50c7df820ebcc043fd30e879
Giovanny Perez <789@holbertonschool.com>
2019-02-13 20:57:37 +0000

    Commit Double

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,35 +1,45 @@
 #include <stdio.h>
 
 /**
- * main - print first 50 Fibonacci starting 1 & 2
+ * main - print first 100 Fibonacci starting 1 & 2
  *
  * Return: Always 0.
  */
 int main(void)
 {
 	int cont;
-	int a1, a2, b1, b2, c , suma1, suma2;
+	long  a1, a2, b1, b2, c, suma1, suma2;
 
 	a1 = 0;
 	a2 = 1;
 	b1 = 0;
 	b2 = 2;
 	c = 0;
+	suma1 = 0;
+	suma2 = 0;
 
-	printf("%d, %d, ",a2 ,b2 );
-	for (cont = 0; cont <= 93; cont++)
+	printf("%ld, %ld, ", a2, b2);
+	for (cont = 0; cont <= 96; cont++)
 	{
-		suma1 = a1 + b1;
-		if (suma1 > 10000001)
-			c = (suma1 / 1000000); 
-		suma1 = suma1 - c;
-		suma2 = a2 + b2 + c;
+		c = 0;
+		suma2 = a2 + b2;
+		if (suma2 > 99999999999)
+			c = 1;
+		suma2 = suma2 - (c * 100000000000);
+		suma1 = a1 + b1 + c;
 		a1 = b1;
 		a2 = b2;
 		b1 = suma1;
 		b2 = suma2;
-		printf("%d:%d, ", suma1, suma2);
+		if (suma1 == 0)
+			printf("%ld, ", suma2);
+		else
+		{
+			if (cont != 96)
+				printf("%ld%ld, ", suma1, suma2);
+			else
+				printf("%ld%ld\n", suma1, suma2);
+		}
 	}
-/*	printf("%ld\n", a + b); */
 	return (0);
 }
