194ebd071f6aab098a33dd9f27154d65c624c5e5
Giovanny Perez <789@holbertonschool.com>
2019-02-20 03:05:29 +0000

    Commit Pointer

--- 0x04-pointers_arrays_strings/8-print_array.c
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -15,11 +15,11 @@ void print_array(int *a, int n)
 	if (n != 0)
 	{
 		for (i = 1; i < n ; i++)
-			{
-				printf("%d, ", *a);
-				a++;
-			}
-			printf("%d\n", *a);
+		{
+		printf("%d, ", *a);
+		a++;
+		}
+		printf("%d\n", *a);
 	}
 	else
 	printf("\n");
