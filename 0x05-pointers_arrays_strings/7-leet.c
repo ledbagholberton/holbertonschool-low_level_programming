d854dd627e5fa02b59ddf01ff16178bfbd57d9c8
Giovanny Perez <789@holbertonschool.com>
2019-02-21 23:52:55 +0000

    pointers2

---
+++ 0x05-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * leet - encodes in 1337
+ *
+ * @str: Address of string
+ * Return: Return pointer to string
+ */ 
+char *leet(char *str)
+{
+	char *aux;
+	char src[] = "aeotlAEOTL";
+	char dest[] = "4307143071";
+	int cont, cont2;
+
+	cont = 0;
+	aux = str;
+	while(aux[cont] != '\0')
+	{
+		for (cont2 = 0; src[cont2] != '\0'; cont2++)
+		{
+			if (aux[cont] == src[cont2])
+				aux[cont] = dest[cont2];
+		}
+		cont++;
+	}
+	return (str);
+}
