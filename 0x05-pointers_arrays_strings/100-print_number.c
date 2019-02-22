f00e4751203a5815d73de8d0576bf6c34608d464
Giovanny Perez <789@holbertonschool.com>
2019-02-22 05:02:42 +0000

    Pointers

--- 0x05-pointers_arrays_strings/100-print_number.c
+++ 0x05-pointers_arrays_strings/100-print_number.c
@@ -14,19 +14,28 @@ void print_number(int n)
 	num_b = n;
 	divisor = 1;
 	cont = 0;
-	if (n < 0)
-		_putchar('-');
-	while (num_a > 0)
+	if (n != 0)
 	{
-		num_a = num_a / divisor;
-		cont++;
-		divisor = divisor * 10;
+		if (num_a < 0)
+			_putchar('-');
+		while (num_a != 0)
+		{
+			num_a = num_a / 10;
+			cont++;
+			divisor = divisor * 10;
+		}
+		cont = cont-1;
+		divisor = divisor / 10;
+		for (a = 0; a <= cont; a++)
+		{
+			b = num_b / divisor;
+			if (b < 0)
+				b = -b;
+			_putchar(b + '0');
+			num_b = num_b % divisor;
+			divisor = divisor / 10;
+		}
 	}
-	divisor = divisor / 10;
-	for (a = 0; a <= cont; a++)
-	{
-		b = num_b / divisor;
-		_putchar(b + '0');
-		num_b = num_b % divisor;
-	}
+	else
+		_putchar('0');
 }
