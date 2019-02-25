abbc679e2517ceaad4e22aa5adc7d45e216c1974
Giovanny Perez <789@holbertonschool.com>
2019-02-25 22:24:07 +0000

    Arrays

--- 0x06-pointers_arrays_strings/7-print_chessboard.c
+++ 0x06-pointers_arrays_strings/7-print_chessboard.c
@@ -14,10 +14,10 @@ void print_chessboard(char (*a)[8])
 	for (rows = 0; rows <= 7; rows++)
 	{
 		for (columns = 0; columns <= 7; columns++)
-	     {
-		     letra = a[rows][columns];
-		     _putchar(letra);
-	     }
+		{
+		letra = a[rows][columns];
+		_putchar(letra);
+		}
 		_putchar('\n');
 	}
 }
