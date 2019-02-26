0acb0113a2f6b05bbe2980ec1c1496e56dd5de82
Giovanny Perez <789@holbertonschool.com>
2019-02-26 04:28:14 +0000

    pointers

--- 0x06-pointers_arrays_strings/5-strstr.c
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -13,11 +13,17 @@ char *_strstr(char *haystack, char *needle)
 
 	for (cont = 0; haystack[cont] != '\0'; cont++)
 	{
-		for (cont2 = 0; needle[cont] != '\0'; cont2++)
+		for (cont2 = 0; *needle(cont2) != '\0'; cont2++)
 		{
-			if (haystack[cont] == needle[cont2])
-			{
-				flag++;
+		}
+		for (cont3 = 0; *needle(cont2) == *haystack(cont + cont2) ; cont3++)
+		{
+		cont2++
+					
+			
+
+
+
 			}
 		}
 		if (flag = cont2)
---
+++ 0x06-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * set_string - Change the pointer to string s0 to s1
+ *
+ * @s: Pointer to pointer
+ * @to: Pointer to dest
+ * Return: No return
+ */
+
+void set_string(char **s, char *to)
+{
+	to = s; 
+}
