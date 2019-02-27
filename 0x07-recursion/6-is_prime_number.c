d495d27c8d34e2e98514ae038539ed72034d5a5a
Giovanny Perez <789@holbertonschool.com>
2019-02-27 21:28:29 +0000

    Recursive

--- 0x07-recursion/6-is_prime_number.c
+++ 0x07-recursion/6-is_prime_number.c
@@ -6,6 +6,31 @@
  * Return: 1 if the number is prime / 0 else
  */
 
+int se_divide(int n, int x)
+{
+	if (x < n)
+	{
+		if (n%x == 0)
+			return (0);
+		else
+			return (se_divide(n, x+1));
+	}
+	else
+		return (1);
+}
+
 int is_prime_number(int n)
 {
-	if (n % 
+	int a, x;
+
+	x = 2;
+	if (n <= 1)
+		a = 0;
+	else
+	{
+		a = se_divide(n,x);
+		if (a > 0)
+			a = 1;
+	}
+	return (a);
+} 
