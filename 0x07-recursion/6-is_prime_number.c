d611017df55bf4035b336cde735482476e315c0d
Giovanny Perez <789@holbertonschool.com>
2019-02-27 21:36:03 +0000

    Recursive

--- 0x07-recursion/6-is_prime_number.c
+++ 0x07-recursion/6-is_prime_number.c
@@ -1,23 +1,30 @@
 #include "holberton.h"
 /**
- * is_prime_number - Define if n is prime or not
+ * se_divide - Define if n is divisible by x
  *
  * @n: Number to evaluate
- * Return: 1 if the number is prime / 0 else
+ * @x: Number to divide
+ * Return: 0 if is divisible / 1 otherwise
  */
 
 int se_divide(int n, int x)
 {
 	if (x < n)
 	{
-		if (n%x == 0)
+		if (n % x == 0)
 			return (0);
 		else
-			return (se_divide(n, x+1));
+			return (se_divide(n, x + 1));
 	}
 	else
 		return (1);
 }
+/**
+ * is_prime_number - Define if n is prime or not
+ *
+ * @n: Number to evaluate
+ * Return: 1 if the number is prime / 0 else
+ */
 
 int is_prime_number(int n)
 {
@@ -28,9 +35,9 @@ int is_prime_number(int n)
 		a = 0;
 	else
 	{
-		a = se_divide(n,x);
+		a = se_divide(n, x);
 		if (a > 0)
 			a = 1;
 	}
 	return (a);
-} 
+}
