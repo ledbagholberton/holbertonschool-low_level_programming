90bb07ffb1de060bdc8705844d891078b1179c7b
Giovanny Perez <789@holbertonschool.com>
2019-02-28 00:48:00 +0000

    Message

--- 0x07-recursion/1-print_rev_recursion.c
+++ 0x07-recursion/1-print_rev_recursion.c
@@ -1,17 +1,15 @@
 #include "holberton.h"
-
 /**
- * _print_rev_recursion - Print a string in reverse
+ * _print_rev_recursion - REturns reverse string
  *
- * @s: Pointer of string
+ * @s: Pointer to string
  * Return: No return
  */
 
 void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
-	{
-	_print_rev_recursion(s + 1);
-	}
-	_putchar(*(s - 1));
+		_print_rev_recursion(s+1);
+	s--;
+	_putchar(*s);
 }
