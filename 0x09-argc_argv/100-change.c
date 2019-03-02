70e90b19fa9e223cb108938f49de01a503bb3847
Giovanny Perez <789@holbertonschool.com>
2019-03-02 00:08:37 +0000

    Change

---
+++ 0x09-argc_argv/100-change.c
@@ -0,0 +1,55 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ *@argc: Counts arguments in argv
+ *@argv: Strings after exe
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int a25, b25, a10, b10, a5, b5, a2, b2, a1, total, suma;
+
+	argc = argc;
+	if (argc == 1)
+	{
+		printf("Error\n");
+		return(1);
+	}
+	else
+	{	
+		if (atoi(argv[1]) < 0)
+		{
+		printf("0\n");
+		return (0);
+		}
+		else
+		{
+			total = atoi(argv[1]);
+			if (argc > 1 && total != 0 && *argv[1] != '0')
+				{
+					a25 = total / 25;
+					b25 = total % 25;
+					a10 = b25 / 10;
+					b10 = b25 % 10;
+					a5 = b10 / 5;
+					b5 = b10 % 5;
+					a2 = b5 / 2;
+					b2 = b5 % 2;
+					a1 = b2 / 1;
+					suma = a25 + a10 + a5 + a2 + a1;
+					printf("%d\n", suma);
+					return (0);
+				}
+				else
+				{
+					printf("Error\n");
+					return (1);
+				}
+		}
+	}	
+}
