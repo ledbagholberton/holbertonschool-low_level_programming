7992e613cccd1496b278444b1ce537d07ef1d55f
Giovanny Perez <789@holbertonschool.com>
2019-03-03 23:30:38 +0000

    Add

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -12,13 +12,27 @@
 
 int main(int argc, char *argv[])
 {
-	int cont, suma;
+	int cont, cont2, suma;
+	char *cadena;
 
 	argc = argc;
 	suma = 0;
+
 	for (cont = 1; cont < argc; cont++)
 	{
+		cadena = argv[cont];
+		for (cont2 = 0; cadena[cont2] != '\0'; cont2++)
+		{
+			if (cadena[cont2] < 48 || cadena[cont2] > 57)
+			{
+				printf ("Error\n");
+				return (1);
+			}
+		}
+	}
 
+	for (cont = 1; cont < argc; cont++)
+	{
 		if (atoi(argv[cont]) != 0 && *argv[cont] != '0')
 		{
 			suma = suma + atoi(argv[cont]);
