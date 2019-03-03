216cec4eadb9bc3bea1bf8903478c93943ebc82e
Giovanny Perez <789@holbertonschool.com>
2019-03-03 03:42:34 +0000

    message

--- 0x09-argc_argv/100-change.c
+++ 0x09-argc_argv/100-change.c
@@ -20,41 +20,30 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return(1);
 	}
-	else
-	{	
-		if (atoi(argv[1]) < 0)
-		{
+	if (atoi(argv[1]) < 0)
+	{
 		printf("0\n");
 		return (0);
-		}
-		if (atoi(argv[1] = 0)
-		{
+	}
+	if (atoi(argv[1]) == 0)
+	{
 		printf("Error\n");
 		return (0);
-		}
-		else
-		{
-			total = atoi(argv[1]);
-			if (total != 0)
-				{
-					a25 = total / 25;
-					b25 = total % 25;
-					a10 = b25 / 10;
-					b10 = b25 % 10;
-					a5 = b10 / 5;
-					b5 = b10 % 5;
-					a2 = b5 / 2;
-					b2 = b5 % 2;
-					a1 = b2 / 1;
-					suma = a25 + a10 + a5 + a2 + a1;
-					printf("%d\n", suma);
-					return (0);
-				}
-			else
-				{
-					printf("Error\n");
-					return (1);
-				}
-		}
-	}	
+	}
+	total = atoi(argv[1]);
+	if (total != 0)
+	{
+		a25 = total / 25;
+		b25 = total % 25;
+		a10 = b25 / 10;
+		b10 = b25 % 10;
+		a5 = b10 / 5;
+		b5 = b10 % 5;
+		a2 = b5 / 2;
+		b2 = b5 % 2;
+		a1 = b2 / 1;
+		suma = a25 + a10 + a5 + a2 + a1;
+		printf("%d\n", suma);
+		return (0);
+	}
 }
