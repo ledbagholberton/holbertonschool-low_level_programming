032232df10884c04e4c759e6764f90bbb7aa76fb
Giovanny Perez <789@holbertonschool.com>
2019-03-05 18:17:34 +0000

    Allocation

--- 0x0A-malloc_free/1-strdup.c
+++ 0x0A-malloc_free/1-strdup.c
@@ -1,6 +1,5 @@
 #include "holberton.h"
 #include <stdlib.h>
-#include <stdio.h>
 
 /**
  * _strdup - CLone astring in a memory space
@@ -19,13 +18,13 @@ char *_strdup(char *str)
 	for (cont = 0; str[cont] != '\0'; cont++)
 	{
 	}
-/*	printf("LOngitud %d\n",cont);*/
 	newstr = malloc(cont * sizeof(char));
 	if (newstr == NULL)
 		return (NULL);
-	for (cont2 = 0; cont2 <= cont + 1; cont2++)
+	for (cont2 = 0; cont2 <= cont; cont2++)
 	{
 		newstr[cont2] = str[cont2];
 	}
+	newstr[cont2] = '\0';
 	return (newstr);
 }
