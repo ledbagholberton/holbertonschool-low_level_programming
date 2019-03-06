c92e1968963836b9ac0a2e08614d703032103922
Giovanny Perez <789@holbertonschool.com>
2019-03-06 03:14:36 +0000

    Allocation

---
+++ 0x0A-malloc_free/5-argstostr.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+char *argstostr(int ac, char **av)
+{
+	int i,j,cont,size;
+	char *buffer;
+
+	size = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			size++;
+		}
+	}
+	buffer = malloc(size + ac);
+	if (buffer == NULL)
+		return (NULL);
+	j = 0;
+	cont = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			buffer[cont] = av[i][j];
+			cont++;
+		}
+		buffer[cont] = '\n';
+		cont++;
+	}
+	return (buffer);
+}
