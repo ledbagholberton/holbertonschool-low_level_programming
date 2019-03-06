743d3161602b4169ce7a00d6a9c176186ef618c3
Giovanny Perez <789@holbertonschool.com>
2019-03-06 04:43:04 +0000

    Advanced

--- 0x0A-malloc_free/100-strtow.c
+++ 0x0A-malloc_free/100-strtow.c
@@ -5,15 +5,15 @@
 /**
  * free_all - free memory space in case of failure
  * @buffer: Space to free
- * @height: Height of grid
+ * @cont: counter
  *
  * Return: Pointer double to grid.
  */
 
-void free_all(char **buffer, int cont)                
+void free_all(char **buffer, int cont)
 {
 	int i;
-	
+
 	for (i = 0; i < cont; i++)
 		free(buffer[i]);
 	free(buffer);
@@ -38,30 +38,30 @@ char **strtow(char *str)
 				cont++;
 		}
 	cont++;
-	array_str = malloc(sizeof(char*) * cont);
+	array_str = malloc(sizeof(char *) * cont);
 	if (array_str == NULL)
-	       return(NULL);
-	
+		return (NULL);
+
 	for (j = 0; str[j] != '\0'; j++)
-                for (i = 0; str[i] != ' '; i++)
-                {
-                        if (str[i - 1] >= 33 || str[i - 1] < 126)
+		for (i = 0; str[i] != ' '; i++)
+		{
+			if (str[i - 1] >= 33 || str[i - 1] < 126)
 			{
 				for (k = 0; str[i + k] != ' ' || str[i + k] != '\0'; k++)
 					cont_word++;
 				array_str[j] = malloc(sizeof(char) * cont_word);
 				if (array_str[j] == NULL)
-					    {
-						    free_all(array_str, cont);
-						    return(NULL);
-					    }
-			}	
-                }
+				{
+				free_all(array_str, cont);
+				return (NULL);
+				}
+			}
+		}
 
 	cont = 0;
 	for (j = 0; str[j] != '\0'; j++)
-                for (i = 0; str[i] != ' '; i++)
-                {
+		for (i = 0; str[i] != ' '; i++)
+		{
 			array_str[j][i] = str[cont];
 			cont++;
 		}
