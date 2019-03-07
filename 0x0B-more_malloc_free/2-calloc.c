0e603363e48dc5ab7a2d0506946b9ff6c7425a45
Giovanny Perez <789@holbertonschool.com>
2019-03-07 18:18:00 +0000

    Allocation

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -5,18 +5,21 @@
  * @nmemb: #s de members
  * @size: the size of the memory to print
  *
- * Return: Nothing.
+ * Return: Pointer to array of integers.
  */
 
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *memory;
+	char *memory;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	memory = malloc(nmemb * size);
 	if (memory == NULL)
 		return (NULL);
+	for (i = 0; i < nmemb * size; i++)
+		memory[i] = '\0';
 	return (memory);
 }
