fe2bddada3d1b678fe010440d2ba1e7cf35ce7c4
Giovanny Perez <789@holbertonschool.com>
2019-03-07 03:05:41 +0000

    avanzada

--- 0x0A-malloc_free/100-strtow.c
+++ 0x0A-malloc_free/100-strtow.c
@@ -2,23 +2,6 @@
 #include <stdlib.h>
 
 /**
- * free_all - free memory space in case of failure
- * @buffer: Space to free
- * @cont: counter
- *
- * Return: Pointer double to grid.
- */
-
-void free_all(char **buffer, int cont)
-{
-	int i;
-
-	for (i = 0; i < cont; i++)
-		free(buffer[i]);
-	free(buffer);
-}
-
-/**
  * strtow - Split a string into words
  * @str: String to split
  *
@@ -33,11 +16,8 @@ char **strtow(char *str)
 	if (str == NULL || *str == '\0')
 		return (NULL);
 	for (j = 0; str[j] != '\0'; j++)
-		if (str[j] == ' ')
-		{
-			if (str[j - 1] >= 33 && str[j - 1] < 126)
-				cont++;
-		}
+		if (str[j] == ' ' && str[j - 1] >= 33 && str[j - 1] < 126)
+			cont++;
 	cont++;
 	array_str = malloc(sizeof(char *) * cont);
 	if (array_str == NULL)
