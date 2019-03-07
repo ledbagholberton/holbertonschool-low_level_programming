19f7ab7df0fea84d111650030461ce12ec183aab
Giovanny Perez <789@holbertonschool.com>
2019-03-07 17:27:21 +0000

    Allocation

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -16,6 +16,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i, a, b, size, pos;
 	char *new_str;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+
 	for (a = 0; s1[a] != '\0'; a++)
 	{
 	}
