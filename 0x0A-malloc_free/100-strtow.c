d4833b80ee3d8498932d03e7759c2ba1299a3cec
Giovanny Perez <789@holbertonschool.com>
2019-03-07 02:18:32 +0000

    Advanced

--- 0x0A-malloc_free/100-strtow.c
+++ 0x0A-malloc_free/100-strtow.c
@@ -1,6 +1,5 @@
 #include "holberton.h"
 #include <stdlib.h>
-#include <stdio.h>
 
 /**
  * free_all - free memory space in case of failure
@@ -28,42 +27,42 @@ void free_all(char **buffer, int cont)
 
 char **strtow(char *str)
 {
-	int i, j, k, cont, cont_word;
+	int k, i = 0, j, flag, cont = 0, cont_w = 0;
 	char **array_str;
 
 	for (j = 0; str[j] != '\0'; j++)
-		for (i = 0; str[i] != ' '; i++)
+		if (str[j] == ' ')
 		{
-			if (str[i - 1] >= 33 || str[i - 1] < 126)
+			if (str[j - 1] >= 33 && str[j - 1] < 126)
 				cont++;
 		}
 	cont++;
 	array_str = malloc(sizeof(char *) * cont);
 	if (array_str == NULL)
 		return (NULL);
-
-	for (j = 0; str[j] != '\0'; j++)
-		for (i = 0; str[i] != ' '; i++)
+	array_str[cont - 1] = NULL;
+	j = 0;
+	for (; str[j] != '\0'; j++)
+	{
+		flag = 0;
+		cont_w = 0;
+		while (str[j + cont_w] != ' ' && str[j + cont_w + 1] != ' '
+		       && str[j + cont_w + 1] != '\0')
 		{
-			if (str[i - 1] >= 33 || str[i - 1] < 126)
-			{
-				for (k = 0; str[i + k] != ' ' || str[i + k] != '\0'; k++)
-					cont_word++;
-				array_str[j] = malloc(sizeof(char) * cont_word);
-				if (array_str[j] == NULL)
-				{
-				free_all(array_str, cont);
+			cont_w++;
+			flag = 1;
+		}
+		if (flag == 1)
+		{
+			array_str[i] = malloc(sizeof(char) * cont_w + 2);
+			if (array_str[i] == NULL)
 				return (NULL);
-				}
-			}
+			for (k = 0; k <= cont_w; k++, j++)
+				array_str[i][k] = str[j];
+			i++;
+			if (i == cont)
+				break;
 		}
-
-	cont = 0;
-	for (j = 0; str[j] != '\0'; j++)
-		for (i = 0; str[i] != ' '; i++)
-		{
-			array_str[j][i] = str[cont];
-			cont++;
-		}
+	}
 	return (array_str);
 }
