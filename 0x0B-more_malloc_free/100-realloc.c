a8f2d925415a2ab23eacd98326beb818693f8512
Giovanny Perez <789@holbertonschool.com>
2019-03-08 06:16:19 +0000

    Ahora si

--- 0x0B-more_malloc_free/100-realloc.c
+++ 0x0B-more_malloc_free/100-realloc.c
@@ -26,7 +26,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 	a = malloc(new_size);
 	if (a == NULL)
-                return (NULL);
+		return (NULL);
 	if (ptr != NULL)
 	{
 		b = ptr;
