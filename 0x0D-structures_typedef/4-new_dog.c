a77e49f8c33b4249bcd76ee9d8f67c15af66ec71
Giovanny Perez <789@holbertonschool.com>
2019-03-12 00:50:53 +0000

    FRRR

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -1,6 +1,34 @@
 #include "dog.h"
 #include <stdlib.h>
 /**
+ * _strdup - CLone astring in a memory space
+ * @str: String to be duplicated
+ *
+ * Return: Pointer to new string.
+ */
+
+char *_strdup(char *str)
+{
+	char *newstr;
+	int cont, cont2;
+
+	if (!str)
+		return (NULL);
+	for (cont = 0; str[cont] != '\0'; cont++)
+	{
+	}
+	newstr = malloc(cont * sizeof(char) + 1);
+	if (newstr == NULL)
+		return (NULL);
+	for (cont2 = 0; cont2 <= cont; cont2++)
+	{
+		newstr[cont2] = str[cont2];
+	}
+	newstr[cont2] = '\0';
+	return (newstr);
+}
+
+/**
  * new_dog - create a new dog memory space
  *
  *@name: name of dog
@@ -12,7 +40,7 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	int i;
+/*	int i; */
 	dog_t *the_dog_ptr;
 	char *copy_name, *copy_owner;
 
@@ -21,20 +49,20 @@ dog_t *new_dog(char *name, float age, char *owner)
 	{
 		return (NULL);
 	}
-	copy_name = malloc(sizeof(*name));
+	copy_name = _strdup(name);
 	if (copy_name == NULL)
 	{
 		free(the_dog_ptr);
 		return (NULL);
 	}
-	copy_owner = malloc(sizeof(*owner));
+	copy_owner = _strdup(owner);
 	if (copy_owner == NULL)
 	{
 		free(the_dog_ptr);
 		free(copy_name);
 		return (NULL);
 	}
-	for (i = 0; name[i] != '\0'; i++)
+/*	for (i = 0; name[i] != '\0'; i++)
 	{
 		copy_name[i] = name[i];
 	}
@@ -44,6 +72,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		copy_owner[i] = owner[i];
 	}
 	copy_owner[i] = '\0';
+*/
 	the_dog_ptr->name = copy_name;
 	the_dog_ptr->owner = copy_owner;
 	the_dog_ptr->age = age;
