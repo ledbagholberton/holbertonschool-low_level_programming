f4bcbb54f3d8596ab269d1cc6e61e5016c0bd822
Giovanny Perez <789@holbertonschool.com>
2019-03-13 17:01:01 +0000

    Functions Pointer

---
+++ 0x0E-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - print a name depending the function pointer called
+ * @name: name of the person
+ * @f : pointer to function
+ * Return: Nothing.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
---
+++ 0x0E-function_pointers/2-int_index.c
@@ -0,0 +1,32 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - search for an integer based on cmp pointer
+ *
+ * @array: where to search the integer
+ * @size:  size of array
+ * @cmp: pointer to function to be executed
+ *
+ * Return: 0 if false, something else otherwise.
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int a, i;
+
+	if (size <= 0)
+		return (-1);
+	i = 0;
+	while (i < size)
+	{
+		a = (*cmp)(array[i]);
+		if (a == 1)
+			return (i);
+		else
+			i++;
+	}
+	return(-1);
+}
+
+
+
