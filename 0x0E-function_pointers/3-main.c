90fc2f5027fcc0d08cdcc367731489aa81dffc09
Giovanny Perez <789@holbertonschool.com>
2019-03-13 21:25:05 +0000

    Calc

---
+++ 0x0E-function_pointers/3-main.c
@@ -0,0 +1,40 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Calc the operation in args
+ *
+ * @argc: Number of arguments
+ * @argv: Inputs Num1, op & Num2
+ */
+
+
+
+int main(int argc, char *argv[])
+{
+	char *s;
+	int num1, num2, res;
+
+	if (argc != 4)
+	{
+		printf("Error de cantidad de argumentos\n");
+		exit(98);
+	}
+	if ((*argv[2] == '/' || *argv[2] == '%') && (atoi(argv[3]) == 0))
+	{
+		printf("Error division\n");
+		exit(100);
+	}
+	if (!(*argv[2] == '+' || *argv[2] == '-' || *argv[2] == '*'
+	     || *argv[2] == '/' || *argv[2] == '%'))
+	{
+		printf("Error de operador\n");
+		exit(99);
+	}
+	s = argv[2];
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	res = (*get_op_func(s))(num1, num2);
+	printf("%d\n", res);
+}
---
+++ 0x0E-function_pointers/3-op_functions.c
@@ -0,0 +1,66 @@
+#include "3-calc.h"
+
+/**
+ * op_add - add two numbers
+ * @a: Number a
+ * @b: Number b
+ *
+ * Return: Add a + b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - substract two numbers
+ * @a: Number a
+ * @b: Number b
+ *
+ * Return: Substraction a - b
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - a times b
+ * @a: Number a
+ * @b: Number b
+ *
+ * Return: Multiplication a * b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divdide two numbers
+ * @a: Number a
+ * @b: Number b
+ *
+ * Return: Div a / b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_add - Module two numbers
+ * @a: Number a
+ * @b: Number b
+ *
+ * Return: a % b
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
