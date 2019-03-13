9a70b3404915893605b76dc113928576c431bebd
Giovanny Perez <789@holbertonschool.com>
2019-03-13 22:10:08 +0000

    Calc

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -26,10 +26,10 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 	if ((*argv[2] == '/' || *argv[2] == '%') && (atoi(argv[3]) == 0))
-        {
-                printf("Error\n");
-                exit(100);
-        }
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	s = argv[2];
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
