bd1759be80727517fd4e28ed4e8e452e2fd86a6b
Giovanny Perez <789@holbertonschool.com>
2019-03-13 22:00:36 +0000

    Calc

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -9,8 +9,6 @@
  * @argv: Inputs Num1, op & Num2
  */
 
-
-
 int main(int argc, char *argv[])
 {
 	char *s;
@@ -21,17 +19,17 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	if ((*argv[2] == '/' || *argv[2] == '%') && (atoi(argv[3]) == 0))
-	{
-		printf("Error\n");
-		exit(100);
-	}
-	if (!(*argv[2] == '+' || *argv[2] == '-' || *argv[2] == '*'
-	     || *argv[2] == '/' || *argv[2] == '%'))
+	if  ((argv[2][1] != '\0') || (!(*argv[2] == '+' || *argv[2] == '-' ||
+		*argv[2] == '*' || *argv[2] == '/' || *argv[2] == '%')))
 	{
 		printf("Error\n");
 		exit(99);
 	}
+	if ((*argv[2] == '/' || *argv[2] == '%') && (atoi(argv[3]) == 0))
+        {
+                printf("Error\n");
+                exit(100);
+        }
 	s = argv[2];
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
