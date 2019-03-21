c53f593a8290585dfd8bfc62b4b65f06eff422b7
Giovanny Perez <789@holbertonschool.com>
2019-03-21 20:47:04 +0000

    Add Nodes linked list

--- 0x11-singly_linked_lists/2-add_node.c
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -11,12 +11,17 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
+	int cont;
 
+	for (cont = 0; str[cont] != '\0'; cont++)
+	{
+	}
+ 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
 	(*new).str = strdup(str);
-	(*new).len = strlen(str);
+	(*new).len = cont;
 	(*new).next = *head;
 	*head = new;
 	return (*head);
