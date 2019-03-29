39bce1a413262b3944acbc43d5669e15a5a0e221
Giovanny Perez <789@holbertonschool.com>
2019-03-29 00:35:37 +0000

    fiix

--- 0x13-bit_manipulation/0-binary_to_uint.c
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,5 @@
 #include "holberton.h"
-
+#include <stdio.h>
 
 /**
  * binary_to_uint - convert string with 0 &1 to integer.
@@ -10,26 +10,25 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int res = 0, mul = 1;
-	int cont;
+	unsigned int res = 0;
+	int cont, mask;
 
 	if (b == NULL)
 		return (0);
-	for (cont = 1; b[cont] != '\0'; cont++)
-	{
-	}
+	for (cont = 0; b[cont] != '\0'; cont++)
+		;
 	cont--;
+	mask = 1;
 	while (cont >= 0)
 	{
-		mul = mul * 2;
-		if (b[cont] == '0' || b[cont] == '1')
-		{
-			res = (res + (b[cont] - '0') * mul);
-			cont--;
-		}
+		if (b[cont] == '1')
+			res = res + mask;
+		else if (b[cont] == '0')
+			res = res;
 		else
 			return (0);
-
+		mask = mask << 1;
+		cont--;
 	}
-	return (res / 2);
+	return (res);
 }
