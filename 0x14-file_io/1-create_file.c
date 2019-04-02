fac0e797581929d8de6b3acb7a392f131345c3a8
Giovanny Perez <789@holbertonschool.com>
2019-04-02 20:15:42 +0000

    File IO 1

---
+++ 0x14-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include "holberton.h"
+/**                                                                                                                                                                                                               
+ * read_textfile- read and sent to stdout a number of charss.
+ *
+ *@filename: name of file
+ *@letters: number of letters
+ * Return: Number of printed chars.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, number, leido;
+
+	number = 0;
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	if (text_content == NULL)
+	{
+		close (fd);
+		return (1);
+	}
+	for (leido = 0; text_content[leido] != '\0'; leido++)
+		;
+	number = write(fd, text_content, leido);
+	if (number == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return (1);
+}
--- 0x14-file_io/holberton.h
+++ 0x14-file_io/holberton.h
@@ -8,7 +8,7 @@
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
-
+int create_file(const char *filename, char *text_content);
 
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
