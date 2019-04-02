2068bbaa0a100180bda354accb6f482be6094af5
Giovanny Perez <789@holbertonschool.com>
2019-04-02 18:43:14 +0000

    File IO

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -23,9 +23,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	leido = read(fd, buf, letters + 1);
 	if (leido == -1)
-		return(0);
+		return (0);
 	buf[letters + 1] = '\0';
 	number = write(1, buf, leido + 1);
+	if (number == -1)
+		return (0);
 	free(buf);
 	close(fd);
 	return (number);
