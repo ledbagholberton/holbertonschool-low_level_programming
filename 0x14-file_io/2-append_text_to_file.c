4aa1f84b3329854ab8cdda09c1989cebed817162
Giovanny Perez <789@holbertonschool.com>
2019-04-02 21:17:23 +0000

    Fix IO 2

---
+++ 0x14-file_io/2-append_text_to_file.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * append_text_to_file- add text at the end of file
+  *
+  *@filename: name of file
+  *@text_content: text to add
+ * Return: 1 if success / -1 if fail.
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, leido, number;
+
+	number = 0;
+	if (filename  == NULL)
+		return (-1);
+	fd = open(filename, O_RDWR | O_APPEND);
+	if (fd == -1)
+		return (-1);
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (-1);
+	}
+	for (leido = 0; text_content[leido] != '\0'; leido++)
+		;
+	write(fd, text_content, leido);
+	if (number == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return (1);
+}
--- 0x14-file_io/holberton.h
+++ 0x14-file_io/holberton.h
@@ -9,7 +9,7 @@
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
-
+int append_text_to_file(const char *filename, char *text_content);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
