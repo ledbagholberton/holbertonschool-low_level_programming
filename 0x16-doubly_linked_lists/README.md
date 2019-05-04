9f8d4a3a676e1a781c4a4ae270919a320c470035
Giovanny Perez <789@holbertonschool.com>
2019-05-04 07:38:21 +0000

    doublelink

---
+++ 0x16-doubly_linked_lists/README.md
@@ -0,0 +1,1 @@
+Here you will find C Code for Double Linked lists
---
+++ 0x16-doubly_linked_lists/pruebas.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import dis
+from magic_calculation_102 import add, sub
+if a<b:
+add(a, b)
+elif:
+sub(a, b)
+dis.dis (magic_calculation_102)
+
