71f5ae2ffb839bd029d75f70e4f590bd1d9bc35f
Giovanny Perez <789@holbertonschool.com>
2019-05-06 02:49:55 +0000

    Fix error in head == null

--- 0x16-doubly_linked_lists/6-main.c
+++ 0x16-doubly_linked_lists/6-main.c
@@ -17,7 +17,7 @@ int main(void)
 	add_dnodeint_end(&head, 0);
 	add_dnodeint_end(&head, 1);
 	add_dnodeint_end(&head, 2);
-	add_dnodeint_end(&head, 3);
+	add_dnodeint_end(&head, 350);
 	add_dnodeint_end(&head, 4);
 	add_dnodeint_end(&head, 98);
 	add_dnodeint_end(&head, 402);
