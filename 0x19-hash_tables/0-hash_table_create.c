f9983bfc6d12dbf772fc7590947ada65a748546c
Giovanny Perez <789@holbertonschool.com>
2019-05-31 19:29:35 +0000

    FIX initialization of array in NULL

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -1,6 +1,5 @@
 #include "hash_tables.h"
 
-
 /**
  * hash_table_create - create a hash table
  * @size: size of hash array
@@ -11,9 +10,20 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_ht;
+	unsigned long int i;
 
 	new_ht = (hash_table_t *) malloc(sizeof(hash_table_t));
+	if (new_ht == NULL)
+		return (NULL);
 	new_ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (new_ht->array == NULL)
+		return (NULL);
+	i = 0;
+	while (i < size)
+	{
+		(*new_ht).array[i] = NULL;
+		i++;
+	}
 	new_ht->size = size;
 	return (new_ht);
 }
