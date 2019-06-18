0d98d4b77f23564d372902a09e74682a881489f4
Abdel <789@holbertonschool.com>
2019-06-18 20:07:16 +0000

    Betty & others in 3-quick_Sort.c

--- 0x1B-sorting_algorithms/3-quick_sort.c
+++ 0x1B-sorting_algorithms/3-quick_sort.c
@@ -1,6 +1,11 @@
 #include "sort.h"
 #include <stdio.h>
-
+/**
+ * quick_sort - Algorithm quick_sort
+ *@array: array to order
+ *@size: size of array
+ * Return: Non return
+ */
 void quick_sort(int *array, size_t size)
 {
 	int low_pos, high_pos, size_low, size_high, pivot;
@@ -18,7 +23,14 @@ void quick_sort(int *array, size_t size)
 			quick_sort(&(array[pivot + 1]), size_high);
 	}
 }
-
+/**
+ * lomuto - partition algorithm
+ *@array: array
+ *@low_pos: low position
+ *@high_pos: high pos
+ *@size: size
+ * Return: Pivot
+ */
 int lomuto(int *array, int low_pos, int high_pos, size_t size)
 	{
 		int i, j, pivot;
@@ -31,18 +43,27 @@ int lomuto(int *array, int low_pos, int high_pos, size_t size)
 			{
 				i++;
 				swap_list(array, i, j);
+				if (i + 1 != j)
+					print_array(array, size);
 			}
 		}
 		pivot = i + 1;
 		swap_list(array, pivot, high_pos);
-		printf("SIZE = %lui\n",size);
-		print_array(array, size);
-		return(pivot);
+		if (i - 1 != pivot)
+			print_array(array, size);
+		return (pivot);
 	}
-
+/**
+ * swap_list - Swap
+ *@array: array
+ *@pos1: pos1
+ *@pos2: pos2
+ * Return: Non return
+ */
 void swap_list(int *array, int pos1, int pos2)
 {
 	int tmp = array[pos1];
+
 	array[pos1] = array[pos2];
 	array[pos2] = tmp;
 }
